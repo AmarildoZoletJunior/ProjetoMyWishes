1949d807a54bafc61653cdf1773425b7a205570c
Amarildo Antonio Zolet Junior <amarildozj@gmail.com>
2022-09-24 14:51:51 -0300

    Removendo arquivos de link

--- lib/dbhelp/constant.dart
+++ lib/dbhelp/constant.dart
@@ -2,7 +2,6 @@
 //user = "Amarildozj";
 //password = "Junior123"
 
-const MONGO_CONN_URL = "mongodb+srv://Amarildozj:Junior123@amarildozj.8w9jmqu.mongodb.net/mydatabase?retryWrites=true&w=majority";
-const USER_COLLECTION = "flutter";
-
+const MONGO_CONN_URL =
+const USER_COLLECTION =
 
